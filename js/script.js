27fd8ca036fab5c6427ebcb97dad1bdf56bf9f12
Jay <ihearthuskiez@gmail.com>
2024-10-01 07:33:25 -0600

    solar system site

---
+++ js/script.js
@@ -0,0 +1,8 @@
+class planet {
+    constructor(name, desc, type, imageURL) {
+        this.name = name;
+        this.desc = desc;
+        this.type = type;
+        this.img = imageURL;
+    }
+}
